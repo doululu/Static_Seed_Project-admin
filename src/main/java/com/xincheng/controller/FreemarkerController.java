2e1a159043b9c4199e73800e5a4720b53777a1e8
doululu <doululu@yidankeji.com>
2018-08-31 18:10:21 +0800

    Initial commit

---
+++ src/main/java/com/xincheng/controller/FreemarkerController.java
@@ -0,0 +1,26 @@
+package com.xincheng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author 豆璐璐
+ * @description
+ * @company 易单科技
+ * @date 2018/8/30 0030 16:16
+ * @Version 1.0
+ */
+@Controller
+public class FreemarkerController {
+
+    @RequestMapping(value = "/goFreemarker",method = RequestMethod.GET)
+    public String goFreemarker(ModelMap modelMap){
+
+        modelMap.put("name","张三");
+        modelMap.put("sex", 1);    //sex:性别，1：男、 0：女
+
+        return "/freemarkerTest";
+    }
+}
